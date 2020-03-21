ee666b13b6471972c977b6965b2c83a42314f101
Pat White <byelipk@gmail.com>
2020-03-21 07:27:40 -0400

    Wow

--- src/components/combobox/combobox.js
+++ src/components/combobox/combobox.js
@@ -1,26 +1,49 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 import AutosizeInput from "react-input-autosize"
 import { useCombobox } from "downshift"
 
 import _styles from "./combobox.module.scss"
 
+const peopleEndpoint = "https://swapi.co/api/people/"
+
+const useFetch = url => {
+  const [response, setResponse] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    if (!url) return
+
+    setLoading(true)
+    setResponse(null)
+    setError(false)
+
+    fetch(url)
+      .then(resp => resp.json())
+      .then(resp => {
+        setLoading(false)
+        setResponse(resp)
+      })
+      .catch(error => {
+        setError(error)
+        setLoading(false)
+      })
+  }, [url])
+
+  return [response, loading, error]
+}
 const Combobox = () => {
-  const [items, setItems] = useState([
-    "Hello",
-    "Goodbye",
-    "Farewell",
-    "So Long",
-    "So",
-    "Wonderful",
-    "Fun things come all the time",
-    "Memories",
-  ])
+  const [endpoint, setEndpoint] = useState(peopleEndpoint + "?search=darth")
+  const [response, loading, error] = useFetch(endpoint)
+  const items = (response && response.results) || []
+
   const [tokens, setTokens] = useState([])
 
   const {
     isOpen,
     reset,
+    openMenu,
     getLabelProps,
     getMenuProps,
     getInputProps,
@@ -28,39 +51,61 @@ const Combobox = () => {
     highlightedIndex,
     getItemProps,
   } = useCombobox({
-    initialIsOpen: true,
-    defaultIsOpen: true,
+    // initialIsOpen: true,
+    // defaultIsOpen: true,
     items,
+    itemToString: (item) => {
+      return item == null ? "" : item.name
+    },
+    onInputValueChange: ({ inputValue }) => {
+      if (!loading) {
+        setEndpoint(`${peopleEndpoint}?search=${inputValue}`)
+      }
+    },
     onSelectedItemChange: changes => {
       const { selectedItem } = changes
 
       if (selectedItem) {
-        setTokens(tokens => [...tokens, selectedItem])
+        setTokens(tokens => [...tokens, selectedItem.name])
         reset()
       }
     },
   })
 
-  const removeToken = (index) => setTokens(tokens => tokens.filter((_, idx) => idx !== index))
+  const removeToken = index =>
+    setTokens(tokens => tokens.filter((_, idx) => idx !== index))
 
   return (
     <div className={`${_styles.wrapper}`}>
       <label {...getLabelProps()}>Search the Star Wars Universe:</label>
       <div {...getComboboxProps({ className: _styles.combobox })}>
         {tokens.map((t, index) => (
-          <button onClick={() => removeToken(index)} type="button" key={index} className={`${_styles.token}`}>
+          <button
+            onClick={() => removeToken(index)}
+            type="button"
+            key={index}
+            className={`${_styles.token}`}
+          >
             {t}
           </button>
         ))}
 
         <AutosizeInput
-          {...getInputProps()}
+          {...getInputProps({ 
+            onFocus: openMenu 
+          })}
           inputClassName={`${_styles.input}`}
-          placeholder={tokens.length > 0 ? "Type" : "Ex. Luke Skywalker"}
+          placeholder={
+            tokens.length > 0 ? "Use the Force..." : "Ex. Luke Skywalker"
+          }
         />
       </div>
 
-      <ul {...getMenuProps()} className={`${_styles.menu} ${isOpen ? _styles.menuOpen : ''}`}>
+      <ul
+        {...getMenuProps()}
+        className={`${_styles.menu} ${isOpen || loading ? _styles.menuOpen : ""}`}
+      >
+        {loading && <li className={`${_styles.menuItem}`}>Loading...</li>}
         {isOpen &&
           items.map((item, index) => (
             <li
@@ -68,10 +113,10 @@ const Combobox = () => {
               style={
                 highlightedIndex === index ? { backgroundColor: "#bde4ff" } : {}
               }
-              key={`${item}${index}`}
+              key={`${item.name}${index}`}
               {...getItemProps({ item, index })}
             >
-              {item}
+              {item.name}
             </li>
           ))}
       </ul>
